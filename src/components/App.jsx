de5a1df0566e42c402927fdd2b1220e338af05d5
Dmytro Klimchuk <d664433334@gmail.com>
2024-10-31 22:33:06 +0200

    Feedbac Options

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,54 @@
 import Description from "./Description/Description";
 import Feedback from "./Feedback/Feedback";
 import Options from "./Options/Options";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-  return <Description />;
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  useEffect(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (savedFeedback) setFeedback(savedFeedback);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  return (
+    <div>
+      <Description />
+      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} />
+      {totalFeedback > 0 && (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,12 @@
+const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
+  return (
+    <div>
+      <h2>Leave Your Feedback</h2>
+      <button onClick={() => updateFeedback("good")}>Good</button>
+      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+      <button onClick={() => updateFeedback("bad")}>Bad</button>
+      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+    </div>
+  );
+};
+export default Options;
