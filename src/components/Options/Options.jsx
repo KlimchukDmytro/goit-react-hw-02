94b2bc7bd6109ead70ee294d776842944358c1eb
Dmytro Klimchuk <d664433334@gmail.com>
2024-11-01 20:48:48 +0200

    finish

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,23 @@
+import s from "./Options.module.css";
+
 const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
   return (
     <div>
       <h2>Leave Your Feedback</h2>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+      <button className={s.btn} onClick={() => updateFeedback("good")}>
+        Good
+      </button>
+      <button className={s.btn} onClick={() => updateFeedback("neutral")}>
+        Neutral
+      </button>
+      <button className={s.btn} onClick={() => updateFeedback("bad")}>
+        Bad
+      </button>
+      {totalFeedback > 0 && (
+        <button className={s.btn} onClick={resetFeedback}>
+          Reset
+        </button>
+      )}
     </div>
   );
 };
